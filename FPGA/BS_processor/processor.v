ff6dab171aeddff603db086a21eaecb2c3566b4b
Jay Kim <esmussseinhj@gmail.com>
2015-06-17 16:39:15 +0900

    milestone: get a result value from given a pseudo random number

--- FPGA/BS_processor/processor.v
+++ FPGA/BS_processor/processor.v
@@ -27,7 +27,36 @@ module processor(
 	reg [31:0] s_const1;
 	reg [31:0] s_const2;
 	reg [31:0] s_const3;
-
+	
+	wire clk_en;
+	// To test computing Black-Scholes model process except for Gaussian Random Number Generator.
+	wire [31:0] pseudo_grn;
+	// Inputs of each modules in this processor.
+	wire [31:0] const2_mult_din;
+	wire [31:0] exp_din;
+	wire [31:0] const1_mult_din;
+	wire [31:0] k_sub_din;
+	wire [31:0] const3_mult_din;
+	// Result outputs of each modules in this processor.
+	wire [31:0] const2_mult_dout;
+	wire [31:0] exp_dout;
+	wire [31:0] const1_mult_dout;
+	wire [31:0] k_sub_dout;
+	wire [31:0] const3_mult_dout;
+	
+	assign status = state;
+	assign clk_en = 1'b1;
+	// This value represents -1 in IEEE float.
+	assign pseudo_grn = 32'b10111111100000000000000000000000;
+	// Connect input and output of each modules.
+	assign const2_mult_din = pseudo_grn;
+	assign exp_din = const2_mult_dout;
+	assign const1_mult_din = exp_dout;
+	assign k_sub_din = const1_mult_dout;
+	assign const3_mult_din = k_sub_dout[31] ? 32'd0 : k_sub_dout;
+	// Assign final processor value.
+	assign dout = const3_mult_dout;
+	
 	/**
 	 *
 	 * @update state
@@ -101,6 +130,58 @@ module processor(
 		end
 	end
 	
-	assign dout = {state, 28'd0};
+	// Latency: 5 clock cycle.
+	// Supports pipelining.
+	fp_mult const2_mult(
+		.aclr(~nreset),
+		.clk_en(clk_en),
+		.clock(clk),
+		.dataa(s_const2),
+		.datab(const2_mult_din),
+		.result(const2_mult_dout)
+	);
+	
+	// Latency: 17 clock cycle.
+	// Supports pipelining.
+	fp_exp exp(
+		.aclr(~nreset),
+		.clk_en(clk_en),
+		.clock(clk),
+		.data(exp_din),
+		.result(exp_dout)
+	);
+	
+	// Latency: 5 clock cycle.
+	// Supports pipelining.
+	fp_mult const1_mult(
+		.aclr(~nreset),
+		.clk_en(clk_en),
+		.clock(clk),
+		.dataa(s_const1),
+		.datab(const1_mult_din),
+		.result(const1_mult_dout)
+	);
+	
+	// Latency: 7 clock cycle.
+	// Supports pipelining.
+	fp_sub k_sub(
+		.aclr(~nreset),
+		.clk_en(clk_en),
+		.clock(clk),
+		.dataa(s_constK),
+		.datab(k_sub_din),
+		.result(k_sub_dout)
+	);
+	
+	// Latency: 5 clock cycle.
+	// Supports pipelining.
+	fp_mult const3_mult(
+		.aclr(~nreset),
+		.clk_en(clk_en),
+		.clock(clk),
+		.dataa(s_const3),
+		.datab(const3_mult_din),
+		.result(const3_mult_dout)
+	);
 	 
 endmodule
