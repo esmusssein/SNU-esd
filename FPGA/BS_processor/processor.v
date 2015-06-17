24dfcc0b3d913f1f81bf370de2d30b1fa00addf1
Jay Kim <esmussseinhj@gmail.com>
2015-06-17 11:58:35 +0900

    milestone: confirm altera library module pipelining

--- FPGA/BS_processor/processor.v
+++ FPGA/BS_processor/processor.v
@@ -14,12 +14,15 @@ module processor(
 	output [31:0] dout
 );
 
+	wire [31:0] fd1;
+	wire [31:0] fd2;
 	wire [31:0] acc_dout;
-	wire [31:0] acc_dina;
+	reg [31:0] acc_dina;
 	reg [31:0] acc_dinb;
 
 	assign status = 4'b0000;
-	assign acc_dina = 32'b00111111100111100000010000011001;
+	assign fd1 = 32'b00111111100111100000010000011001;  // 1.2345
+	assign fd2 = 32'b01000110010000001110010000000000;  // 12345
 	assign dout = acc_dinb;
 	
 	integer cnt_clk;
@@ -27,13 +30,28 @@ module processor(
 	always @(posedge clk or negedge nreset) begin
 		if (nreset == 1'b0) begin
 			cnt_clk <= 0;
-			acc_dinb <= 32'd0;
 		end else begin
-			if (cnt_clk < 28) begin
+			if (cnt_clk < 10000) begin
 				cnt_clk <= cnt_clk + 1;
-				acc_dinb <= acc_dout;
 			end else begin
 				cnt_clk <= cnt_clk;
+			end
+		end
+	end
+	
+	always @(posedge clk or negedge nreset) begin
+		if (nreset == 1'b0) begin
+			acc_dina <= fd1;
+			acc_dinb <= 32'd0;
+		end else begin
+			if (cnt_clk == 1) begin
+				acc_dina <= fd2;
+				acc_dinb <= 32'd0;
+			end else if (cnt_clk < 16) begin
+				acc_dina <= fd2;
+				acc_dinb <= acc_dout;
+			end else begin
+				acc_dina <= acc_dina;
 				acc_dinb <= acc_dinb;
 			end
 		end
