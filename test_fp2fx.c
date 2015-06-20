2dbb386157ebd2f17883cc8a539e3f08ce4c5eb2
Jay Kim <jaykim@Jays-MacBook-Pro.local>
2015-06-20 14:23:50 +0900

    add module test_fp2fx.c

--- test_fp2fx.c
+++ test_fp2fx.c
@@ -2,7 +2,7 @@
 
 typedef int fix16;
 
-#define FRACT_OFFSET 15
+#define FRACT_OFFSET 14
 #define FLOAT2FIXED(x) ((int)((x) * (1 << FRACT_OFFSET)))
 #define FIXED2FLOAT(x) (((float)(x)) / (1 << FRACT_OFFSET))
 
@@ -16,7 +16,7 @@ char *float2bytes(float f) {
 int main() {
     char buf[sizeof(float)];
     // Test.
-    float fp = 123.456;
+    float fp = 3.1622777;
     fix16 fx = FLOAT2FIXED(fp);
     printf("fp %x fx %x fxconv %x\n", *(int *)float2bytes(fp), fx, *(int *)float2bytes(FIXED2FLOAT(fx)));
 
